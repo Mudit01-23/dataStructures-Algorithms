43f7d97c66e7b240922cc54760ba0fe79f5a5e7e
Mudit01-23 <muditjoshi0305@gmail.com>
2025-01-01 14:46:46 +0530

    Deleted testFile.java,testFile2.java and testFile3.java

--- sortingAlgorithms/testFile2.java
+++
@@ -1,5 +0,0 @@
-public class testFile2{
-    public static void main(String[] args) {
-        System.out.println("This is test file no. 2");
-    }
-}
--- sortingAlgorithms/testFile3.java
+++
@@ -1,5 +0,0 @@
-public class testFile3 {
-    public static void main(String[] args) {
-        System.out.println("This is test file number 3");
-    }
-}
