af12f9f85d47cf64e51a4ae16d8cc97a5c317739
Mudit01-23 <muditjoshi0305@gmail.com>
2025-01-03 16:46:22 +0530

    added 2 files inside insertionSort folder

---
+++ sortingAlgorithms/insertionSort/insertionSortUsingFunction.java
@@ -0,0 +1,31 @@
+package insertionSort;
+
+import java.util.Arrays;
+
+public class insertionSortUsingFunction{  
+    public static void main(String[] args) {
+        //int[] array = {5, 4, 3, 2, 1};
+        int[] array = {5,5,4, 4, 3, 2, 1};
+        System.out.println(Arrays.toString(array));
+        insertionSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    static void insertionSort(int[] array){
+       for (int i = 0; i < array.length-1; i++) {
+        for (int j = i+1; j >0; j--) {
+            if(array[j]<array[j-1]){
+                swap(array, j,j-1);
+            }
+            else{
+                break;
+            }
+        }
+       }
+    }
+    static void swap(int[] array, int first, int second){
+        int temp = array[first];
+        array[first]= array[second];
+        array[second] = temp;
+    }
+}
