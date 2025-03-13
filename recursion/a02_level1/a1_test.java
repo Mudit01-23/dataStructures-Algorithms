5eb03fdf5e9e0f4f076fcf0a16497394e5e72646
Mudit01-23 <muditjoshi0305@gmail.com>
2025-03-13 23:51:06 +0530

    adding few files

---
+++ recursion/a02_level1/a1_test.java
@@ -0,0 +1,19 @@
+
+public class a1_test{
+    public static void main(String[] args) {
+       int n =5;
+       func(n);
+       
+
+
+    }
+
+     static void func(int n){
+       if(n==0){
+        return;
+       }
+       func(n-1);
+       System.out.println(n);
+       
+    }
+}
