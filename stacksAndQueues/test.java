cf49cd0d17e2ad29b960f18677e6ff73b3e5cc25
Mudit01-23 <muditjoshi0305@gmail.com>
2025-07-11 02:16:29 +0530

    commiting after long time

---
+++ stacksAndQueues/test.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class test {
+    public static void main(String[] args) {
+
+        // stack 
+        
+        // Stack<Integer> stack = new Stack<>();
+        // System.out.println(stack.push(3));
+        // System.out.println(stack);
+        // System.out.println(stack.pop());
+        
+
+    }
+}
